5d441ac48e0bd7c6492058f9657c03310a83190e
Andrew West <captainrex2000@gmail.com>
2015-01-07 09:08:12 -0800

    first

---
+++ public_html/scripts.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$('body').off('.data-api');
+$('body').off('.alert.data-api');
+
+$("document").ready(function(){
+    $(".gaben").css("background-color", "cyan");
+ });
+
